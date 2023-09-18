29e591985cf8ec1f345b311002a9ebc2196debf0
omerbenarieh <omerrbenarieh@gmail.com>
2023-09-18 17:21:54 +0300

    Settings Feature

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,21 +1,69 @@
+import { useSettings } from './useSettings.js';
+
 import Form from '../../ui/Form.jsx';
 import FormRow from '../../ui/FormRow.jsx';
 import Input from '../../ui/Input.jsx';
+import Spinner from '../../ui/Spinner.jsx';
+import { useUpdateSetting } from './useUpdateSetting.js';
 
 function UpdateSettingsForm() {
+  const {
+    isLoading,
+    settings: {
+      minBookingLength,
+      maxBookingLength,
+      maxGuestsPerBooking,
+      breakfastPrice,
+    } = {},
+  } = useSettings();
+
+  const { isUpdating, updateSetting } = useUpdateSetting();
+
+  if (isLoading) return <Spinner />;
+
+  function handleUpdate(e, field) {
+    const { value } = e.target;
+    if (!value) return;
+    updateSetting({ [field]: value });
+  }
+
   return (
     <Form>
       <FormRow label="Minimum nights/booking">
-        <Input type="number" id="min-nights" />
+        <Input
+          type="number"
+          id="min-nights"
+          defaultValue={minBookingLength}
+          disabled={isUpdating}
+          onBlur={e => handleUpdate(e, 'minBookingLength')}
+        />
       </FormRow>
       <FormRow label="Maximum nights/booking">
-        <Input type="number" id="max-nights" />
+        <Input
+          type="number"
+          id="max-nights"
+          defaultValue={maxBookingLength}
+          disabled={isUpdating}
+          onBlur={e => handleUpdate(e, 'maxBookingLength')}
+        />
       </FormRow>
       <FormRow label="Maximum guests/booking">
-        <Input type="number" id="max-guests" />
+        <Input
+          type="number"
+          id="max-guests"
+          defaultValue={maxGuestsPerBooking}
+          disabled={isUpdating}
+          onBlur={e => handleUpdate(e, 'maxGuestsPerBooking')}
+        />
       </FormRow>
       <FormRow label="Breakfast price">
-        <Input type="number" id="breakfast-price" />
+        <Input
+          type="number"
+          id="breakfast-price"
+          defaultValue={breakfastPrice}
+          disabled={isUpdating}
+          onBlur={e => handleUpdate(e, 'breakfastPrice')}
+        />
       </FormRow>
     </Form>
   );
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,7 +1,14 @@
-import Heading from "../ui/Heading";
+import UpdateSettingsForm from '../features/settings/UpdateSettingsForm.jsx';
+import Heading from '../ui/Heading';
+import Row from '../ui/Row.jsx';
 
 function Settings() {
-  return <Heading as="h1">Update hotel settings</Heading>;
+  return (
+    <Row>
+      <Heading as="h1">Update hotel settings</Heading>
+      <UpdateSettingsForm />
+    </Row>
+  );
 }
 
 export default Settings;
